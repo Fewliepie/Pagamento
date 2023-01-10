d47dad191bfe7f4d23271e94375cfa1bcce96be3
Felipe Araujo Nascimento <felipe.nascimento@credz.com.br>
2023-01-10 20:58:32 -0300

    Criação das classes

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// Boleto; Cartão; Pix; Dinheiro
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Pix pix = new Pix();
+            Console.WriteLine(pix.Mensagem());
+            Console.ReadKey();
+        }
+    }
+
+abstract class Pagamento
+{
+    public virtual string Mensagem()
+    {
+        return "Método inexistente.";
+    }
+}
+class Boleto : Pagamento
+    {
+        public override string Mensagem()
+        {
+            string result = base.Mensagem();
+            result = "Método: boleto.";
+            return result;
+        }
+
+    }
+class Cartão : Pagamento
+{
+    public override string Mensagem()
+    {
+        string result = base.Mensagem();
+        result = "Método: Cartão.";
+        return result;
+    }
+}
+class Pix : Pagamento
+{
+    public override string Mensagem()
+    {
+        string result = base.Mensagem();
+        result = "Método: Pix.";
+        return result;
+    }
+}
+class Dinheiro : Pagamento
+{
+    public override string Mensagem()
+    {
+        string result = base.Mensagem();
+        result = "Método: Dinheiro.";
+        return result;
+    }
+}
